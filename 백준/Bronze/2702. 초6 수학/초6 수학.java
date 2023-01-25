2aa7cfb2eb5ee68d88b19d7806a0e9214b3f2eeb
sangxxjin <sj_0368@naver.com>
2023-01-25 20:34:30 +0900

    Revert "Delete 백준/Bronze/2702. 초6 수학 directory"
    This reverts commit 9375c5ada7b961281c58e79e4c833b1095fa136e.

---
+++ 백준/Bronze/2702. 초6 수학/초6 수학.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        int n = input.nextInt();
+
+        for (int i = 0; i < n; ++i) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            int k = GCD(a, b);
+            System.out.print(a * b / k + " ");
+            System.out.println(k);
+        }
+
+    }
+
+    public static int GCD(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+
+        return GCD(b, a % b);
+
+    }
+}
+
