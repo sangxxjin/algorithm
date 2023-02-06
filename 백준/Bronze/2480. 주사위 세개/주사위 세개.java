01c4c23b3c0f33ae262b1c55831aeeadc1011e18
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2023-02-06 14:30:08 +0900

    [Bronze IV] Title: 주사위 세개, Time: 212 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        if(A==B&B==C) System.out.println(10000+A*1000);
+        else if (A==B) System.out.println(1000+A*100);
+        else if (A==C) System.out.println(1000+A*100);
+        else if (B==C) System.out.println(1000+B*100);
+        else if(A!=B||B!=C||C!=A)
+             if(A>B) System.out.println(Math.max(A,C)*100);
+             else System.out.println(Math.max(B,C)*100);
+    }
+}
