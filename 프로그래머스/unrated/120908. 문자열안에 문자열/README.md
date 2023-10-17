8cc0038b257c7fb8e64275d7661492d3a28c261e
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2023-10-17 10:08:18 +0900

    [level unrated] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120908. 문자열안에 문자열/README.md
+++ 프로그래머스/unrated/120908. 문자열안에 문자열/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 문자열안에 문자열 - 120908 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120908) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120908#) 
 
 ### 성능 요약
 
-메모리: 72.7 MB, 시간: 0.03 ms
+메모리: 73.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 10:6:51
+2023년 10월 2일 10:8:16
 
 ### 문제 설명
 
--- 프로그래머스/unrated/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/unrated/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,16 +1,6 @@
 class Solution {
   public int solution(String str1, String str2) {
-    int answer = 2;
-    for (int i = 0; i <= str1.length()-str2.length(); i++) {
-      if (str1.charAt(i)!=str2.charAt(0)){
-        continue;
-      }
-      int count=0;
-      for (int j = 0; j < str2.length(); j++) {
-        if (str1.charAt(i+j)==str2.charAt(j))count++;
-      }
-      if (count==str2.length())answer=1;
-    }
-    return answer;
+    if(str1.contains(str2))return 1;
+    else return 2;
   }
 }
