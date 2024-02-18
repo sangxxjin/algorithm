a11b2d32b3dd3bbdebf26ab63b87282e7dd5fcda
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2024-02-18 14:53:15 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.91 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        for(int i=0; i<n; i++){
+        String num = "";
+        for(int j=n-1; j>=0; j--){
+            if(((arr1[i] & (1 << j)) | (arr2[i] & (1 << j))) != 0){
+                num += "#";
+            }
+            else{
+                num += " ";
+            }
+        }
+        answer[i] = num;
+    }      
+        return answer;
+    }
+}
