f84f87df2de8b4e8d2b5802949dba53fd47dca2c
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2024-02-28 10:04:27 +0900

    [level 0] Title: 약수 구하기, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(n%i==0)answer.add(i);
+        }
+        return answer;
+    }
+}
