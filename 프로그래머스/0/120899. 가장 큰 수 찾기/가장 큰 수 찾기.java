8f9dc4f39e4f447103d44274a6eec775bd39cf25
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2024-03-27 09:15:58 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+     public int[] solution(int[] array) {
+        int maxIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        return new int[]{array[maxIndex], maxIndex};
+    }
+}
