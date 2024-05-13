4cc90a45ef7cdff82e4b813f0242252d5c0c79fb
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2024-05-14 00:29:43 +0900

    [Silver III] Title: N과 M (4), Time: 796 ms, Memory: 28348 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static int N, M;
+    private static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new int[M];
+        dfs(1, 0);
+    }
+
+    private static void dfs(int at, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = at; i <= N; i++) {
+            arr[depth] = i;
+            dfs(i, depth + 1);
+        }
+    }
+}
