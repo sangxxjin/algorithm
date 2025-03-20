115347526b72b2d43a47c5bb6aa092385440757e
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-03-20 14:19:37 +0900

    [level 1] Title: 하샤드 수, Time: 1.34 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,17 +1,14 @@
 class Solution {
-    public static boolean solution(int x) {
-        boolean answer = true;
-        int org = x;
-        int count = 0;
-        while (x>=1){
-            count+=x%10;
-            x/=10;
+    public boolean solution(int x) {
+        int sum = 0;
+        int originalX = x;
+        while(x > 0){
+            sum += x % 10;
+            x /= 10;
         }
-        if (org%count != 0 )answer=false;
-        return answer;
-    }
 
-    public static void main(String[] args) {
-        System.out.println(solution(10));
+        if(originalX % sum != 0) return false;
+
+        return true;
     }
 }
