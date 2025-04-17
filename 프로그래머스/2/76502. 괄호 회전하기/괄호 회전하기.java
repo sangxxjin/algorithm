3887dfd0bdda4de8ae8583ef4f07b89a179e126d
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-04-17 16:31:54 +0900

    [level 2] Title: 괄호 회전하기, Time: 33.97 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            String rotated = s.substring(i) + s.substring(0, i);
+            if (isCorrect(rotated)) answer++;
+        }
+        return answer;
+    }
+
+    private boolean isCorrect(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (isOpen(cur)) {
+                stack.push(cur);
+            } else {
+                if (stack.isEmpty()) return false;
+                if (!isSet(stack.peek(), cur)) return false;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean isOpen(char c){
+        return c=='('||c=='{'||c=='[';
+    }
+
+    private boolean isSet(char a, char b){
+        return (a=='(' && b==')') || (a=='{' && b=='}') || (a=='[' && b==']');
+    }
+}
