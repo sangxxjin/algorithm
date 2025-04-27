3286a339baf4e96b68d9782bad57c8dd60f5cb0e
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-04-28 02:35:03 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 21.21 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,17 +1,17 @@
 class Solution {
-  public static String solution(int[] food) {
-    String answer = "";
-    for(int i=0; i<food.length; i++){
-      int div = food[i]/2;
-      while(div>0){
-        answer+=i;
-        div--;
-      }
-    }answer+="0";
-    for(int i = answer.length()-2; i>=0; i--){
-      answer+=answer.charAt(i);
+    public String solution(int[] food) {
+        String answer = "";
+        for(int i=1; i<food.length; i++){
+            for(int j=0; j<food[i]/2; j++){
+                answer+=i;                
+            }
+            
+        }
+        return makeResult(answer);
     }
-    return answer;
-  }
-
+    private String makeResult(String s) {
+        StringBuffer sb = new StringBuffer(s);
+        String sbReverse = sb.reverse().toString();
+        return s + "0" + sbReverse;
+    }
 }
