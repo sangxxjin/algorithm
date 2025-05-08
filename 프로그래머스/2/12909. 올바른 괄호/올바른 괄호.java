53f3f403c2627f33e1183ac973c18550a6228e63
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-05-08 22:31:17 +0900

    [level 2] Title: 올바른 괄호, Time: 18.06 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,15 +1,16 @@
 import java.util.*;
 class Solution {
     boolean solution(String s) {
+        boolean answer = true;
         Stack<Character> stack = new Stack<>();
         for(char c : s.toCharArray()){
             if(c=='(')stack.push(c);
-            if(c==')'){
+            else{
                 if(stack.isEmpty())return false;
                 stack.pop();
             }
         }
-        if(stack.isEmpty())return true;
-        return false;
+        if(!stack.isEmpty())return false;
+        return answer;
     }
 }
