42a3a2deb5802c837cfbcaa2a8025a8fa8b83a1c
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-05-16 01:03:53 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,34 +1,31 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
-		int M = in.nextInt();
-		int[] arr = new int[N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextInt();
+		public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            int[] arr = new int[N];
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            int max = 0;
+            for(int i=0; i<N-2; i++){
+                if(arr[i]>M)continue;
+                for(int j=i+1; j<N-1; j++){
+                    if((arr[i]+arr[j])>M)continue;
+                    for(int k=j+1; k<N; k++){
+                        if((arr[i]+arr[j]+arr[k])<= M &&(arr[i]+arr[j]+arr[k])>max ){
+                            max = arr[i]+arr[j]+arr[k];
+                        }
+                    }
+                }
+            }
+            System.out.println(max);
 		}
-		int result = search(arr, N, M);
-		System.out.println(result);
 	}
-	static int search(int[] arr, int N, int M) {
-		int result = 0;
-		for (int i = 0; i < N - 2; i++) {
-			for (int j = i + 1; j < N - 1; j++) {
-				for (int k = j + 1; k < N; k++) {
-					
-					int temp = arr[i] + arr[j] + arr[k];
-					
-					if (M == temp) {	
-						return temp;
-					}
-					if(result < temp && temp < M) {
-						result = temp;
-					}
-				}
-			}
-		}
-		
-		return result;
-	}
-}
