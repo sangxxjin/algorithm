16a49959b5089e8e84cb3f25a0d48b91bf239f32
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-05-25 23:54:09 +0900

    [level 1] Title: 예산, Time: 0.55 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,16 +1,14 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
-  public int solution(int[] d, int budget) {
-    Arrays.sort(d);
-    int answer = 0;
-    for (int i = 0; i < d.length; i++) {
-      budget-=d[i];
-      if (budget<0){
-        break;
-      }
-      answer++;
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for(int i=0; i<d.length; i++){
+            if(d[i]<=budget){
+                answer++;
+                budget-=d[i];
+            }
+        }
+        return answer;
     }
-    return answer;
-  }
 }
