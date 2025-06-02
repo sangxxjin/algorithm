a1941b20c007d7336a7d7b659a05c8322bdb9aef
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-06-02 22:49:49 +0900

    [level 2] Title: 요격 시스템, Time: 408.15 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] targets) {
+        Arrays.sort(targets, (a, b) -> Integer.compare(a[1], b[1]));
+
         int answer = 0;
-        
-        Arrays.sort(targets, (o1, o2) -> o1[1] - o2[1]);
-        
-        int lastEndTime = 0;
-        for(int[] target : targets){
-            if(lastEndTime <= target[0]){
-                lastEndTime = target[1];
+        int lastEnd = -1;
+
+        for (int[] target : targets) {
+            if (target[0] >= lastEnd) {
                 answer++;
+                lastEnd = target[1];
             }
         }
-        
+
         return answer;
     }
 }
