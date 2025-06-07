574d9519d0cfec9a610941d99c59c2bb68f48fd1
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-06-07 13:25:56 +0900

    [level 1] Title: 모의고사, Time: 0.77 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,45 +1,23 @@
 import java.util.*;
 class Solution {
     public List<Integer> solution(int[] answers) {
-        List<Integer> arr = new ArrayList<>();
-        int[] r = new int[]{first(answers),second(answers),third(answers)};
-        int max=Math.max(r[0],Math.max(r[1],r[2]));
-        for(int i=0; i<r.length; i++){
-            if(r[i]==max)arr.add(i+1);
-        }
-        return arr;
-    }
-    private int first(int[] answers){
-        int[] sub = new int[]{1,2,3,4,5};
-        int sum=0;
-        int idx=0;
+        int[] first = new int[]{1,2,3,4,5};
+        int[] second = new int[]{2,1,2,3,2,4,2,5};
+        int[] third = new int[]{3,3,1,1,2,2,4,4,5,5};
+        int firstAnswerCount =0;
+        int secondAnswerCount =0;
+        int thirdAnswerCount =0;
         for(int i=0; i<answers.length; i++){
-            if(answers[i]==sub[idx++]) sum++;
-            if(idx>4)idx=0;
+            if(answers[i]==first[i%5]) firstAnswerCount++;
+            if(answers[i]==second[i%8]) secondAnswerCount++;
+            if(answers[i]==third[i%10]) thirdAnswerCount++;
         }
-        return sum;
-    }
-    private int second(int[] answers){
-        // 21232425 반복
-        int[] sub = new int[]{2,1,2,3,2,4,2,5};
-        int sum=0;
-        int idx=0;
-        for(int i=0; i<answers.length; i++){
-            if(answers[i]==sub[idx++]) sum++;
-            if(idx>7)idx=0;
+        int max = Math.max(firstAnswerCount,Math.max(secondAnswerCount,thirdAnswerCount));
+        int[] countList = new int[]{firstAnswerCount,secondAnswerCount,thirdAnswerCount};
+        List<Integer> answer = new ArrayList<>();
+        for(int i =0; i<countList.length; i++){
+            if(max==countList[i])answer.add(i+1);
         }
-        return sum;
-    }
-    private int third(int[] answers){
-        // 33  11 22 44 55 반복
-        // 21232425 반복
-        int[] sub = new int[]{3,3,1,1,2,2,4,4,5,5};
-        int sum=0;
-        int idx=0;
-        for(int i=0; i<answers.length; i++){
-            if(answers[i]==sub[idx++]) sum++;
-            if(idx>9)idx=0;
-        }
-        return sum;
+        return answer;
     }
 }
