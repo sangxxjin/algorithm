5c9a6d8b4199701423ff6e834e0ba35c66e57990
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-06-13 17:29:09 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.80 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,23 +1,20 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;
 class Solution {
-  public int[] solution(String s) {
-    int[] answer = new int[s.length()];
-    Map<Character, Integer> map = new HashMap<>();
-
-    for (int i = 0; i < s.length(); i++) {
-      if (!map.containsKey(s.charAt(i))) {
-        answer[i] = -1;
-        map.put(s.charAt(i), i);
-      } else {
-        answer[i] = i - map.get(s.charAt(i));
-        map.put(s.charAt(i), i);
-      }
+    public int[] solution(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        int[] answer = new int[s.length()];
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(!map.containsKey(c)){
+                map.put(c,i);
+                answer[i]=-1;
+            }
+            else {
+                answer[i]=i-map.get(c);
+                map.put(c,i);
+                
+            }
+        }
+        return answer;
     }
-
-    return answer;
-  }
 }
