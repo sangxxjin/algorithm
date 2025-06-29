32eeb47e15b896c3626db98a84ec9c9d3084797f
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-06-29 23:45:10 +0900

    [level 2] Title: H-Index, Time: 0.94 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,9 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        
         Arrays.sort(citations);
-		
 		for(int i = 0; i < citations.length; i++) {
 			int h = citations.length - i;
 			
@@ -14,7 +12,6 @@ class Solution {
 				break;
 			}
 		}
-        
         return answer;
     }
 }
